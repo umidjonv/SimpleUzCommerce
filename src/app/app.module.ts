0acca0a29a695c1f19c7b9c19bb91a61421fd7f4
umidjonv <vumid85@mail.ru>
2021-12-31 01:25:37 +0500

    not complete layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { PageComponent } from './layout/page/page.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    SidebarComponent,
+    PageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
+
 export class LayoutComponent implements OnInit {
 
   constructor() { }
